0face7df5d68ec24ba4d257c1f65fe39a0ecc3c5
Nastrodamous <andre@Andres-MacBook-Pro.local>
2016-11-18 00:44:56 -0500

    Adding final tutorial file

--- MyPlayground4.playground/Contents.swift
+++ MyPlayground4.playground/Contents.swift
@@ -2,4 +2,44 @@
 
 import UIKit
 
-var str = "Hello, playground"
+struct TaylorFan {
+    static var favoriteSong = "Shake it Off"
+    
+    var name: String
+    var age: Int
+}
+
+let fan = TaylorFan(name: "James", age: 25)
+print(TaylorFan.favoriteSong)
+
+
+class Album {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+    
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+    
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+}
+
+var taylorSwift = StudioAlbum(name: "Tay Tay", studio: "atl studio")
+var fearless = StudioAlbum(name: "speakNow", studio: "aimee land")
+var itunesLive = LiveAlbum(name: "itunes", location: "live from soho")
+
+    var allAlbums: [Album] = [taylorSwift, fearless,itunesLive]
